7fc2aee5ba8552e1d88d8f7c5f99cdbddf41265f
Mateos Puerta, Sergio <smatpue573@g.educaand.es>
2022-11-15 22:07:08 +0100

    initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  users: any;
+  filtro: string = '';
+
+  constructor(private httpClient: HttpClient) {
+
+    this.users = this.httpClient.get('https://randomuser.me/api/?results=20').pipe(map(res => res['results']));
+
+  }
+    
+}
