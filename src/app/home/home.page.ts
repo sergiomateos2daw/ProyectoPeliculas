de8600fd712b91c637eacdb7f0e6e92f89c6f3bf
Mateos Puerta, Sergio <smatpue573@g.educaand.es>
2022-11-16 12:53:08 +0100

    Json actualizado, homepage v1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,13 +9,13 @@ import { map } from 'rxjs/operators';
 })
 export class HomePage {
 
-  users: any;
+  movies: any;
   filtro: string = '';
 
   constructor(private httpClient: HttpClient) {
 
-    this.users = this.httpClient.get('https://randomuser.me/api/?results=20').pipe(map(res => res['results']));
-
+    this.movies = this.httpClient.get('https://raw.githubusercontent.com/sergiomateos2daw/ProyectoPeliculas/master/peliculas.json');
+    console.log(this.movies);
   }
     
 }
